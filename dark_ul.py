38902c7029c0859ad1035a907320ee26a2fb0275
Brett Morris <brettmorris@menelaus.unibe.ch>
2019-08-29 15:26:44 +0200

    Init commit

---
+++ dark_ul.py
@@ -0,0 +1,19 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.stats import mad_std
+
+from toolkit import (photometry, transit_model_b,
+                     PhotometryResults, PCA_light_curve, params_b)
+from astropy.io import fits
+
+image_paths = sorted(glob('/Users/brettmorris/data/rem/20190829/IMG*UL*.fits'))[:-3]
+
+images = []
+
+for p in image_paths:
+    images.append(fits.getdata(p))
+
+fits.writeto('outputs/masterdark_ul.fits', np.nanmedian(images, axis=0), overwrite=True)
---
+++ tic2242_bl.py
@@ -0,0 +1,77 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.stats import mad_std
+
+from toolkit import (photometry, transit_model_b,
+                     PhotometryResults, PCA_light_curve, params_b)
+
+# Image paths
+image_paths = sorted(glob('/Users/brettmorris/data/rem/20190829/IMG*BL*.fits'))[:-3]
+master_flat_path = 'outputs/master_flat_s_201906_BL_z_1s_norm.fits'
+master_dark_path = 'outputs/masterdark.fits'
+
+# dark = np.zeros_like(fits.getdata(image_paths[0]))
+# fits.writeto(master_dark_path, dark)
+
+# Photometry settings
+aperture_radii = np.arange(15, 30)
+centroid_stamp_half_width = 20
+psf_stddev_init = 2
+aperture_annulus_radius = 50
+transit_parameters = params_b
+star_positions = [[594, 661],
+                  [318, 618]]
+output_path = 'outputs/20190829bl.npz'
+force_recompute_photometry = False #True
+
+# Do photometry:
+
+if not os.path.exists(output_path) or force_recompute_photometry:
+    print('Calculating photometry:')
+    phot_results = photometry(image_paths, master_dark_path, master_flat_path,
+                              star_positions,
+                              aperture_radii, centroid_stamp_half_width,
+                              psf_stddev_init, aperture_annulus_radius,
+                              output_path)
+
+else:
+    phot_results = PhotometryResults.load(output_path)
+
+# print('Calculating PCA...')
+
+lcs = (phot_results.fluxes[:, 0, :]/phot_results.fluxes[:, 1, :])
+norm_lcs = lcs / np.median(lcs, axis=0)
+min_std = np.argmin(mad_std(norm_lcs, axis=0))
+
+# plt.plot(phot_results.times, lcs[:, min_std], '.')
+# plt.show()
+
+
+regressors = np.vstack([phot_results.fluxes[:, 1, min_std],
+                        phot_results.xcentroids[:, 0] - phot_results.xcentroids[:, 0].mean(),
+                        phot_results.ycentroids[:, 0] - phot_results.ycentroids[:, 0].mean(),
+                        phot_results.airmass]).T
+
+target_lc = phot_results.fluxes[:, 0, min_std]
+
+y = np.linalg.lstsq(regressors, target_lc)[0]
+comp_lc = regressors @ y
+
+fig, ax = plt.subplots(4, 1, figsize=(10, 5))
+ax[0].plot(phot_results.times, target_lc / comp_lc, '.')
+ax[1].set_ylabel('X')
+ax[1].plot(phot_results.times, phot_results.xcentroids[:, 0], '.')
+ax[1].plot(phot_results.times, phot_results.xcentroids[:, 1], '.')
+
+ax[2].set_ylabel('Y')
+ax[2].plot(phot_results.times, phot_results.ycentroids[:, 0], '.')
+ax[2].plot(phot_results.times, phot_results.ycentroids[:, 1], '.')
+
+ax[3].set_ylabel('Flux')
+ax[3].plot(phot_results.times, phot_results.fluxes[:, 0, min_std], '.')
+ax[3].plot(phot_results.times, phot_results.fluxes[:, 1, min_std], '.')
+plt.show()
+
